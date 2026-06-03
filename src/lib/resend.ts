90a636c40c24c1e213407cf2ad292e594b1cc8aa
Maxwell Johnpaul <maxwelljohnpaul247@gmail.com>
2026-06-03 09:47:49 +0100

    fix: provide dummy resend key during build

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || "re_dummy_key_to_pass_build");
 
 export const sendEmail = async ({ to, subject, html }: { to: string | string[], subject: string, html: string }) => {
   try {
